6457ec4d676e4344a4803bee9f857f7380cb3dc7
DanishveerSingh <danishveersingh2004@gmail.com>
2025-01-09 18:03:21 +0530

    Delete 13.2.py

--- 13.2.py
+++
@@ -1,5 +0,0 @@
-marks = (9,5,7,3,10,4,6);
-print("Sum of list elements is: ",sum(marks));
-print("Average of list elements is: ",sum(marks)/len(marks));
-print("Maximum element of list is: ",max(marks));
-print("Minimum element of list is: ",min(marks));
