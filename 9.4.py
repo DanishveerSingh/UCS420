3271a24bfc038edf17e47b0a8d171e089022922b
DanishveerSingh <danishveersingh2004@gmail.com>
2025-01-09 18:08:20 +0530

    Delete 9.4.py

--- 9.4.py
+++
@@ -1,7 +0,0 @@
-import random as r;
-import string as s;
-special = "!@#$%^&*()_+:<>?;,.|~";
-passw = r.sample(s.ascii_letters+s.digits+special,10);
-password = "".join(passw);
-print(passw);
-print(password);
